db6998982435266b4e4d6dc4bd240da6c4ec2f10
BaHTy3-PT <Opa.enduro@yandex.ru>
2026-04-09 13:09:11 +0300

    Create script-7-dbit.py

---
+++ get-led/script-7-dbit.py
@@ -0,0 +1,60 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+
+leds = [16, 12, 25, 17, 27, 23, 22, 24]
+
+up = 9   
+down = 10  
+
+GPIO.setup(leds, GPIO.OUT)
+GPIO.output(leds, 0)
+
+GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+num = 0
+sleep_time = 0.2     
+
+def dec2bin(value):
+    """Преобразует число 0-255 в список битов (старший бит первый)"""
+    return [int(bit) for bit in bin(value)[2:].zfill(8)]
+
+try:
+    while True:
+        
+        up_pressed = (GPIO.input(up) == 0)
+        down_pressed = (GPIO.input(down) == 0)
+
+        if up_pressed and down_pressed:
+          
+            num = 255
+            print("RESET TO MAX:", num, dec2bin(num))
+            time.sleep(sleep_time)
+          
+            while GPIO.input(up) == 0 or GPIO.input(down) == 0:
+                time.sleep(0.01)
+        elif up_pressed:
+    
+            num = (num + 1) % 256
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
+            while GPIO.input(up) == 0:
+                time.sleep(0.01)
+        elif down_pressed:
+     
+            num = (num - 1) % 256
+            print(num, dec2bin(num))
+            time.sleep(sleep_time)
+            while GPIO.input(down) == 0:
+                time.sleep(0.01)
+
+
+        GPIO.output(leds, dec2bin(num))
+        time.sleep(0.02)
+
+except KeyboardInterrupt:
+    GPIO.output(leds, 0)
+    GPIO.cleanup()
