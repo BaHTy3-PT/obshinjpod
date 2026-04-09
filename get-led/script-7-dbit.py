7d03f73a09b9d41d40463900a8bad3dcbfffae30
BaHTy3-PT <Opa.enduro@yandex.ru>
2026-04-09 13:16:04 +0300

    Update script-7-dbit.py

--- get-led/script-7-dbit.py
+++ get-led/script-7-dbit.py
@@ -3,10 +3,9 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-
 leds = [16, 12, 25, 17, 27, 23, 22, 24]
 
-up = 9   
+up = 9      
 down = 10  
 
 GPIO.setup(leds, GPIO.OUT)
@@ -16,9 +15,10 @@ GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 num = 0
-sleep_time = 0.2     
+sleep_time = 0.2          
 
 def dec2bin(value):
+    """Преобразует число 0-255 в список битов (старший бит первый)"""
     return [int(bit) for bit in bin(value)[2:].zfill(8)]
 
 try:
@@ -28,28 +28,27 @@ try:
         down_pressed = (GPIO.input(down) == 0)
 
         if up_pressed and down_pressed:
-          
+           
             num = 255
-            print("RESET TO MAX:", num, dec2bin(num))
+            print("MAX:", num, dec2bin(num))
             time.sleep(sleep_time)
           
             while GPIO.input(up) == 0 or GPIO.input(down) == 0:
                 time.sleep(0.01)
         elif up_pressed:
-    
+           
             num = (num + 1) % 256
             print(num, dec2bin(num))
             time.sleep(sleep_time)
             while GPIO.input(up) == 0:
                 time.sleep(0.01)
         elif down_pressed:
-     
+          
             num = (num - 1) % 256
             print(num, dec2bin(num))
             time.sleep(sleep_time)
             while GPIO.input(down) == 0:
                 time.sleep(0.01)
-
 
         GPIO.output(leds, dec2bin(num))
         time.sleep(0.02)
