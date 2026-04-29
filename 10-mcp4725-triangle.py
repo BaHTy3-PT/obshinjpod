6fdef0053af775aeb640521517ea054d996191df
BaHTy3-PT <Opa.enduro@yandex.ru>
2026-04-29 22:38:15 +0300

    Create 10-mcp4725-triangle.py

---
+++ 10-mcp4725-triangle.py
@@ -0,0 +1,18 @@
+import time
+import mcp4725_driver
+import signal_generator as sg
+
+amplitude = 5.0
+signal_freq = 10
+sampling_freq = 200
+t = 0.0
+
+try:
+    dac = mcp4725_driver.MCP4725(5.0, address=0x61)
+    while True:
+        voltage = sg.get_triangle_wave_amplitude(signal_freq, t) * amplitude
+        dac.set_voltage(voltage)
+        sg.wait_for_sampling_period(sampling_freq)
+        t += 1.0 / sampling_freq
+finally:
+    dac.deinit()
