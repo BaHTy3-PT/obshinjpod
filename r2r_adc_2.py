6428520fc46a49e07a643bea50fe3ef82fc3e936
BaHTy3-PT <Opa.enduro@yandex.ru>
2026-05-07 10:50:33 +0300

    Create r2r_adc_2.py

---
+++ r2r_adc_2.py
@@ -0,0 +1,62 @@
+import RPi.GPIO as GPIO
+import time
+
+class R2R_ADC:
+    def __init__(self, dynamic_range, compare_time=0.01, verbose=False):
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+        self.compare_time = compare_time
+        
+        self.bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
+        self.comp_gpio = 21
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.bits_gpio, GPIO.OUT, initial=0)
+        GPIO.setup(self.comp_gpio, GPIO.IN)
+
+    def deinit(self):
+        GPIO.output(self.bits_gpio, 0)
+        GPIO.cleanup()
+
+    def number_to_dac(self, number):
+        for i in range(8):
+            bit = (number >> i) & 1
+            GPIO.output(self.bits_gpio[i], bit)
+
+    def sequential_counting_adc(self):
+        for number in range(256):
+            self.number_to_dac(number)
+            time.sleep(self.compare_time)
+            if GPIO.input(self.comp_gpio) == GPIO.HIGH:
+                return number
+        return 255
+
+    def get_sc_voltage(self):
+        number = self.sequential_counting_adc()
+        voltage = number * self.dynamic_range / 255.0
+        return voltage
+
+    def successive_approximation_adc(self):
+        number = 0
+        for bit in range(7, -1, -1):
+            mask = 1 << bit
+            number |= mask
+            self.number_to_dac(number)
+            time.sleep(self.compare_time)
+            if GPIO.input(self.comp_gpio) == GPIO.HIGH:
+                number &= ~mask
+        return number
+
+    def get_sar_voltage(self):
+        number = self.successive_approximation_adc()
+        voltage = number * self.dynamic_range / 255.0
+        return voltage
+
+if __name__ == "__main__":
+    adc = R2R_ADC(dynamic_range=3.3)
+    try:
+        while True:
+            voltage = adc.get_sar_voltage()
+            print(f"Напряжение: {voltage:.3f} В")
+    finally:
+        adc.deinit()
