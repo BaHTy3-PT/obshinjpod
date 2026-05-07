1e0252e23b91d1adc99f4c939d9bbfd13f609dc7
BaHTy3-PT <Opa.enduro@yandex.ru>
2026-05-07 10:49:25 +0300

    Create adc_plot_2.py

---
+++ adc_plot_2.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def plot_voltage_vs_time(time, voltage, max_voltage):
+    plt.figure(figsize=(10, 6))
+    plt.plot(time, voltage)
+    plt.title("Зависимость напряжения от времени")
+    plt.xlabel("Время, с")
+    plt.ylabel("Напряжение, В")
+    plt.xlim(0, max(time))
+    plt.ylim(0, max_voltage)
+    plt.grid(True)
+    plt.show()
+
+def plot_sampling_period_hist(time):
+    sampling_periods = [time[i+1] - time[i] for i in range(len(time)-1)]
+    plt.figure(figsize=(10, 6))
+    plt.hist(sampling_periods, bins=30)
+    plt.title("Распределение периодов измерений")
+    plt.xlabel("Период измерения, с")
+    plt.ylabel("Количество измерений")
+    plt.xlim(0, 0.06)
+    plt.grid(True)
+    plt.show()
