1d907a08b087a0e895de15dea64aa1f4f44df53b
BaHTy3-PT <Opa.enduro@yandex.ru>
2026-05-07 10:49:55 +0300

    Create r2r-sc_2.py

---
+++ r2r-sc_2.py
@@ -0,0 +1,24 @@
+import time
+from r2r_adc import R2R_ADC
+import adc_plot
+
+DYNAMIC_RANGE = 3.3
+
+adc = R2R_ADC(DYNAMIC_RANGE, compare_time=0.0001)
+
+voltage_values = []
+time_values = []
+duration = 3.0
+
+try:
+    start = time.time()
+    while time.time() - start < duration:
+        voltage = adc.get_sc_voltage()
+        elapsed = time.time() - start
+        voltage_values.append(voltage)
+        time_values.append(elapsed)
+
+    adc_plot.plot_voltage_vs_time(time_values, voltage_values, DYNAMIC_RANGE)
+    adc_plot.plot_sampling_period_hist(time_values)
+finally:
+    adc.deinit()
